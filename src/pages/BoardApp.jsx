8f3dfb193ed31eab8dc3277f26df4267b9a42b94
Asaf <user@Users-MacBook-Pro.local>
2021-06-05 19:41:25 +0300

    new merge demodata

--- src/pages/BoardApp.jsx
+++ src/pages/BoardApp.jsx
@@ -32,7 +32,6 @@ class _BoardApp extends Component {
             // socketService.emit('user watch',loggedInUser._id)
             socketService.emit('join board', board._id)
             socketService.on('board updated', savedBoard => {
-
                 this.props.loadBoard(savedBoard._id)
             })
             this.removeEvent = eventBusService.on('card-edit', ({ elPos, card }) => {
