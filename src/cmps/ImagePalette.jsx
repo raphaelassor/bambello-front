46b3f258987fe97aeb31b4a22ee7f65373a32fe5
adivversano <adivversano@gmail.com>
2021-06-06 19:16:47 +0300

    BACKGROUND SEARCH

--- src/cmps/ImagePalette.jsx
+++ src/cmps/ImagePalette.jsx
@@ -4,7 +4,8 @@ import { unSplashService } from '../services/unsplash.service'
 export class ImagePalette extends Component {
 
     state = {
-        imgs: []
+        imgs: [],
+        keyword: ''
     }
 
     componentDidMount() {
@@ -20,23 +21,47 @@ export class ImagePalette extends Component {
         }
     }
 
+    handleChange = ({ target }) => {
+        const { value } = target
+        this.setState({ keyword: value }, () => {
+            if (value.length >= 3) this.onSearch()
+            else if (value.length === 0) this.loadImgs()
+        })
+    }
+
+    onSearch = async () => {
+        try {
+            const imgs = await unSplashService.searchImgs(this.state.keyword)
+            this.setState({ imgs })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     render() {
-        const { imgs } = this.state
+        const { imgs, keyword } = this.state
         const { handleChange } = this.props
 
         if (!imgs) return <div></div>
 
         return <div className="image-palette">
-            {imgs.map(img => {
-                return <label
-                    key={img.id}
-                    name="label-img"
-                    className="flex align-center justify-center"
-                    style={{ backgroundImage: `url(${img.small})` }}
-                    htmlFor={`img-${img.id}`}>
-                    <input type="radio" name="imgUrl" id={`img-${img.id}`} value={img.full} onClick={handleChange} />
-                </label>
-            })}
+            <div className="image-palette-search">
+                <span className="search-icon"></span>
+                <input type="text" className="search-input" value={keyword} onChange={this.handleChange} onKeyDown={this.handleChange} />
+            </div>
+
+            <div className="images">
+                {imgs.map(img => {
+                    return <label
+                        key={img.id}
+                        name="label-img"
+                        className="flex align-center justify-center"
+                        style={{ backgroundImage: `url(${img.small})` }}
+                        htmlFor={`img-${img.id}`}>
+                        <input type="radio" name="imgUrl" id={`img-${img.id}`} value={img.full} onClick={handleChange} />
+                    </label>
+                })}
+            </div>
         </div>
     }
 }
--- src/services/unsplash.service.js
+++ src/services/unsplash.service.js
@@ -3,16 +3,26 @@ import Axios from 'axios'
 const UNSPLASH_API = 'XRQOpM2i_4jKCx8hYZ-DShQGDeJWMrx6hBqg3tpIgFo';
 
 export const unSplashService = {
-    getTenImgs
+    getTenImgs,
+    searchImgs
 }
 
 async function query(keyword = '16:9') {
-    const res = await Axios.get(`https://api.unsplash.com/search/photos?query=${keyword}&client_id=${UNSPLASH_API}`)
+    const res = await Axios.get(`https://api.unsplash.com/search/photos?query=${keyword}&orientation=landscape&client_id=${UNSPLASH_API}`)
     return res.data
 }
 
 async function getTenImgs() {
     const { results } = await query();
+    return Promise.resolve(
+        results.map(img => {
+            return { id: img.id, small: img.urls.small, full: img.urls.full }
+        })
+    )
+}
+
+async function searchImgs(keyword) {
+    const { results } = await query(keyword);
     return Promise.resolve(
         results.map(img => {
             return { id: img.id, small: img.urls.small, full: img.urls.full }
